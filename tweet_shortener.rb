10892e556f8b1b3760ebb0fd2fce18c871ec5a5b
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 19:32:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,4 +28,4 @@ def word_substituter(tweet)
     string = returnarray.join(" ")
     return string
 end
-word_substituter("")
+word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
