40cea9b059efa361e97afd957e05fb9b74b22eba
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 19:51:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,6 +21,7 @@ def word_substituter(tweet)
 
     array.map! do |x|
       testarray = sub_words.keys
+      puts testarray
       i = 0
       for i in 0..testarray.length-1 do 
         if(x == testarray[i])
